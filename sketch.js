5973638e06e51ae8c2fea08a0d53d30d66723dec
MarionBarthe <marion.barthe@laposte.net>
2023-03-10 15:27:51 +0100

    canvas de 1024x1024 rempli en random

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1024, 1024);
 }
 
 
 function draw() {
   background(200);
+
+  colors =['#000', '#FFF'];
+  for (let i=0; i<1024; i++){
+    for (let j=0; j<1024; j++){
+      let color = random(colors);
+      stroke(color);
+      point(i,j);
+    }
+  }
 }
