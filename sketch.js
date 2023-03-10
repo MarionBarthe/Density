53975da6f1fca85dd57fba8c60f4916133874dd9
MarionBarthe <marion.barthe@laposte.net>
2023-03-10 15:52:09 +0100

    taille canvas maj pour coller à l'oeuvre originale

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
 function setup() {
-  createCanvas(1024, 1024);
+  createCanvas(768, 512);
+  noLoop();
 }
 
 
 function draw() {
   background(200);
 
+  let width = 768;
+  let height = 512;
+
   colors =['#000', '#FFF'];
-  for (let i=0; i<1024; i++){
-    for (let j=0; j<1024; j++){
+  for (let i=0; i<width; i++){
+    for (let j=0; j<height; j++){
       let color = random(colors);
       stroke(color);
       point(i,j);
