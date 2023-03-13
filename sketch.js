0d3704f27902cfdb268a1b7a08fe42f83c68f608
2-1-1-2 <tanyan.francois@gmail.com>
2023-03-13 17:21:28 +0100

    résultat satisfaisant

--- sketch.js
+++ sketch.js
@@ -15,21 +15,16 @@ let colours = [
 ];
 
 
-var data = [  colours[0],colours[0],colours[0],colours[0],colours[0],colours[0],colours[0],colours[0],colours[0],colours[0],colours[0],
-  colours[1],  colours[1],  colours[1],
-  colours[2],  colours[2],
-  colours[3]]; 
-
 let dense_1 = [colours[0],colours[0],colours[0],colours[0],colours[0],colours[3]]
-let dense_2 = [colours[0],colours[0],colours[0],colours[0],colours[3],colours[3]]
-let dense_3 = [colours[0],colours[0],colours[0],colours[3],colours[3],colours[3]]
-let dense_4 = [colours[0],colours[0],colours[3],colours[3],colours[3],colours[3]]
+let dense_2 = [colours[0],colours[0],colours[0],colours[3],colours[3],colours[3]]
+let dense_3 = [colours[0],colours[0],colours[3],colours[3],colours[3],colours[3]]
+let dense_4 = [colours[0],colours[3],colours[3],colours[3],colours[3],colours[3]]
 
 
-let division_X = 16*3*4;
-let division_Y = 16*2*4;
 
-let taille_rect = 4;
+let taille_rect = 1.5;
+let division_X = 16*3*4*2;
+let division_Y = 16*2*4*2;
 
 function push_array(tour){
   let data_ = [];
@@ -45,46 +40,42 @@ function push_array(tour){
   for(let i = 0; i < 1*tour; i++){
     data_.push(1);
   }
+  if(ceil(tour) - tour != 0){
+    let tmp = (int)(Math.random()*4);
+    data_.push(tmp == 0?10:tmp == 1?3:tmp == 2?2:1)
+  }
+
   return data_;
 }
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(division_X*taille_rect, division_Y*taille_rect);
   noStroke()
-  grille()
   
 }
 
 function draw() {
   
-}
-function mousePressed() {
-  setup();
-}
-function grille(parameter_random = 1){
-  background(240);
   let data_1 = push_array(1);
   let superfield = [];
   while (data_1.length) {
     superfield.push(data_1.splice(data_1.length * Math.random() | 0, 1)[0]);
   }
 
-  data_1 = push_array(5);
+  data_1 = push_array(4.75);
   let largefield = [];
-  console.log(largefield)
   while (data_1.length) {
     largefield.push(data_1.splice(data_1.length * Math.random() | 0, 1)[0]);
   }
 
   
-  data_1 = push_array(10);
+  data_1 = push_array(9.75);
   let smallfield = [];
-  console.log(smallfield)
   while (data_1.length) {
     smallfield.push(data_1.splice(data_1.length * Math.random() | 0, 1)[0]);
   }
 
-console.log(smallfield);
+
   for(let i=0; i < division_X; i++){
     for(let j=0; j < division_Y; j++){
       //let ran = data.splice(data.length * Math.random() | 0, 1)[0];
@@ -106,32 +97,41 @@ console.log(smallfield);
       let large = largefield[add_2]
       let small = smallfield[add_3]
 
-      let ran = Math.random()*12;
-      let palette = ran == 0?sup:ran == 1?large:small;
-      switch(palette){
-        case 10:
-          fill(dense_1[(int)(dense_1.length * Math.random())]);
-          break;
-        
-        case 3:
-          fill(dense_2[(int)(dense_2.length * Math.random())]);
-          break;
-        
-        case 2:
-          fill(dense_3[(int)(dense_3.length * Math.random())]);
-          break;
-        
-        case 1:
-          fill(dense_4[(int)(dense_4.length * Math.random())]);
-          break;
-        
-      }
+      let ran = (int)(Math.random()*3);
+      //console.log(ran)
+      let palette = ran == 0?small:ran == 1?large:sup;
+      remplissage(palette);
       
      //fill(ran)
       
       rect(i*taille_rect, j*taille_rect, taille_rect, taille_rect);     
     }  
   }
-
-
+}
+function remplissage(palette){
+  switch(palette){
+        case 10:
+          fill(dense_1[(int)(dense_1.length * Math.random())]);
+          //fill(dense_1[(int)((mouseX * Math.random())%dense_1.length)]);
+          break;
+        
+        case 3:
+          fill(dense_2[(int)(dense_2.length * Math.random())]);
+          //fill(dense_2[(int)((mouseX * Math.random())%dense_2.length)]);
+          break;
+        
+        case 2:
+          fill(dense_3[(int)(dense_3.length * Math.random())]);
+          //fill(dense_3[(int)((mouseX * Math.random())%dense_3.length)]);
+          break;
+        
+        case 1:
+          fill(dense_4[(int)(dense_4.length * Math.random())]);
+          //fill(dense_4[(int)((mouseX * Math.random())%dense_4.length)]);
+          break;
+        
+  }
+}
+function mousePressed() {
+  setup();
 }
